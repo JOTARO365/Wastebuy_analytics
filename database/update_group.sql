47a0f9bb9a55672e5e5c593fe29d7dde25d68cdb
Wisanu Aon-In <108912837+JOTARO365@users.noreply.github.com>
2025-02-25 09:52:42 +0700

    Add files via upload

--- database/update_group.sql
+++ database/update_group.sql
@@ -1,10 +1,5 @@
-insert into customer_groups
-(id, customer_group, id_status, group_code) values
-(39, 'youเทิร์น-KTB', 1, 'G0039'),
-(40, 'youเทิร์น-NARA Thai Cuisine', 1, 'G0040'),
-(41, 'youเทิร์น- Singha Estate', 1, 'G0041'),
-(42, 'B2B-ร้านอาหารBCG', 1, 'G0042'),
-(43, 'Nestle-โครงการกล่องนมรักษ์โลก', 1, 'G0043'),
-(44, 'B2B- KMITL', 1, 'G0044'),
-(45, 'B2B-โฮมเพ้นท์', 1, 'G0045');
-
+insert into customer_groups values 
+(49, 'SWU-คณะวิทยาศาตร์', 1, 'G0049'),
+(50, 'B2B-SPARKOL', 1, 'G0050'),
+(51, 'B2B-การตลาด2', 1, 'G0051'),
+(52, 'youเทิร์น-CPAxtra', 1, 'G0052');
